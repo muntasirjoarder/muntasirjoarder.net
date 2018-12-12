153a31175ed1ec9f5c172f8449b8e2517806e060
Muntasir Joarder <muntasir.joarder@gmail.com>
2018-12-12 14:33:25 +1000

    Google Tag Manager Added. Need to test now.

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,12 @@ module.exports = {
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-KTK7TQT",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
