f7e1cf5acb68bdd65d70db0d9a62568fe4ff906c
Muntasir Joarder <muntasir.joarder@gmail.com>
2024-07-21 08:27:19 +1000

    just change

--- src/pages/index.js
+++ src/pages/index.js
@@ -167,7 +167,7 @@ const IndexPage = () => (
                   <h1 className="mb-0">Muntasir
                     <span className="text-primary">Joarder</span>
                   </h1>
-                  <div className="subheading mb-5">Senior Data Engineer  .  Australia  · 
+                  <div className="subheading mb-5">Senior Data Engineer .  Australia  · 
                     <a href="mailto:muntasir.official@gmail.com">muntasir.official@gmail.com</a>
                   </div>
                   <p className="lead mb-5">Microsoft Certified: Azure Data Engineer Associate with over 15 years of professional experience in IT industry. Expertise in building and enhancing modern data warehouse with medalion architecture using Microsoft Fabric, Azure Synapse Analytics, Azure Data Factory, Data Lake Gen2, Databricks, data build tool (dbt) and Power BI developing metadata-driven pipelines.</p>
